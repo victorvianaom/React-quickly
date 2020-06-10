f165e635d426172ddf7b6a515c6cf775f17b339a
victorvianaom <35640700+victorvianaom@users.noreply.github.com>
2020-06-10 16:28:33 -0300

    i made babel work git add .git add .!

---
+++ Ch-03/src/main.js
@@ -0,0 +1,18 @@
+//@node_modules/babel/preset-react
+
+const E = React.createElement
+h1 = E('h1', null, 'test')
+ReactDOM.render(
+    h1,
+    document.getElementById('content-1')
+)
+
+//comparing JavaScript with JSX
+ReactDOM.render(
+    React.createElement('h1', null, 'Hello!'),
+    document.getElementById('content-2')
+)
+ReactDOM.render(
+    <h1>Hello!</h1>,
+    document.getElementById('content-3')
+)   
