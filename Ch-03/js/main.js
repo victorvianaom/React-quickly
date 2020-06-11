62a2c2808119c74f690f51c9a4b4e2f364334a73
victorvianaom <35640700+victorvianaom@users.noreply.github.com>
2020-06-10 21:11:54 -0300

    printing variables with jsx

--- Ch-03/js/main.js
+++ Ch-03/js/main.js
@@ -19,3 +19,46 @@ let helloWorldReactElement = React.createElement(
     'Heloooooo'
 );
 ReactDOM.render(helloWorldReactElement, document.getElementById('content-4'));
+
+//now components/elements with the use of JSX
+class HelloWorld extends React.Component {
+    render() {
+        return React.createElement(
+            'div',
+            null,
+            React.createElement(
+                'h1',
+                null,
+                '1. Hello mundo'
+            ),
+            React.createElement(
+                'h2',
+                null,
+                '2. Hi there World'
+            )
+        );
+    }
+}
+
+ReactDOM.render(React.createElement(HelloWorld, null), document.getElementById('content-5'));
+
+class DateTimeNow1 extends React.Component {
+    render() {
+        let dateTimeNow = new Date().toLocaleString();
+        return React.createElement('span', null, `Current date and time is ${dateTimeNow}`);
+    }
+}
+ReactDOM.render(React.createElement(DateTimeNow1, null), document.getElementById('content-6'));
+
+class DateTimeNow2 extends React.Component {
+    render() {
+        let dateTimeNow = new Date().toLocaleString();
+        return React.createElement(
+            'span',
+            null,
+            'Current date and time is ',
+            dateTimeNow
+        );
+    }
+}
+ReactDOM.render(React.createElement(DateTimeNow2, null), document.getElementById('content-7'));
--- Ch-03/src/main.js
+++ Ch-03/src/main.js
@@ -23,3 +23,46 @@ ReactDOM.render(
     helloWorldReactElement,
     document.getElementById('content-4')
 )
+
+//now components/elements with the use of JSX
+class HelloWorld extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>1. Hello mundo</h1>
+                <h2>2. Hi there World</h2>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <HelloWorld/>,
+    document.getElementById('content-5')
+)
+
+class DateTimeNow1 extends React.Component {
+    render() {
+        let dateTimeNow = new Date().toLocaleString()
+        return React.createElement(
+            'span',
+            null,
+            `Current date and time is ${dateTimeNow}`
+        )
+    } 
+}
+ReactDOM.render(
+    <DateTimeNow1/>,
+    document.getElementById('content-6')
+)
+
+class DateTimeNow2 extends React.Component {
+    render() {
+        let dateTimeNow = new Date().toLocaleString()
+        return <span>Current date and time is {dateTimeNow}</span>
+    } 
+}
+ReactDOM.render(
+    <DateTimeNow2/>,
+    document.getElementById('content-7')
+)
