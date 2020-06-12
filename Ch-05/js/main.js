e841228de0adaa506cc943d5a0ee63387e7f389d
victorvianaom <35640700+victorvianaom@users.noreply.github.com>
2020-06-12 20:24:05 -0300

    adding and removing event listener as mount and unmount of elements

--- Ch-05/js/main.js
+++ Ch-05/js/main.js
@@ -18,4 +18,106 @@ class Content extends React.Component {
 }
 ReactDOM.render(React.createElement(Content, null), document.getElementById('content-1'));
 
-///////////////////////////////
+/////////////////////////////// USERS
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: []
+        };
+    }
+    componentDidMount() {
+        fetch('./users.json').then(response => response.json()).then(users => this.setState({ users: users }));
+    }
+
+    render() {
+        return React.createElement(
+            'div',
+            { className: 'container' },
+            React.createElement(
+                'h1',
+                null,
+                'List of Users'
+            ),
+            React.createElement(
+                'table',
+                { className: 'table-striped table-condensed table table-bordered table-hover' },
+                React.createElement(
+                    'tbody',
+                    null,
+                    this.state.users.map(user => React.createElement(
+                        'tr',
+                        { key: user.id },
+                        React.createElement(
+                            'td',
+                            null,
+                            user.first_name,
+                            ' ',
+                            user.last_name
+                        ),
+                        React.createElement(
+                            'td',
+                            null,
+                            user.email
+                        ),
+                        React.createElement(
+                            'td',
+                            null,
+                            user.ip_address
+                        )
+                    ))
+                )
+            )
+        );
+    }
+}
+ReactDOM.render(React.createElement(Users, null), document.getElementById('content-2'));
+
+//// Adding and removing an event listener
+class Note extends React.Component {
+    confirmLeave(e) {
+        let confirmationMessage = 'Do you really wanna close?';
+        e.returnValue = confirmationMessage;
+        return confirmationMessage;
+    }
+    componentDidMount() {
+        console.log('Attaching confirmLeave event listener for beforeunload');
+        window.addEventListener('beforeunload', this.confirmLeave);
+    }
+    componentWillUnmount() {
+        console.log('Removing confirmLeave event listener for beforeunload');
+        window.removeEventListener('beforeunload', this.confirmLeave);
+    }
+
+    render() {
+        console.log('Rendering...');
+        return React.createElement(
+            'p',
+            null,
+            'Here will be our input field for notes (parent will remove in ',
+            this.props.secondsLeft,
+            ' seconds)'
+        );
+    }
+}
+
+let secondsLeft = 5;
+let interval = setInterval(() => {
+    if (secondsLeft === 0) {
+        ReactDOM.render(React.createElement(
+            'div',
+            null,
+            'Note was removed after ',
+            secondsLeft,
+            ' seconds.'
+        ), document.getElementById('content-3'));
+        clearInterval(interval);
+    } else {
+        ReactDOM.render(React.createElement(
+            'div',
+            null,
+            React.createElement(Note, { secondsLeft: secondsLeft })
+        ), document.getElementById('content-3'));
+    }
+    secondsLeft--;
+}, 1000);
--- Ch-05/jsx/main.jsx
+++ Ch-05/jsx/main.jsx
@@ -33,4 +33,81 @@ ReactDOM.render(
     document.getElementById('content-1')
 )
 
-///////////////////////////////
+/////////////////////////////// USERS
+class Users extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            users: []
+        }
+    }
+    componentDidMount() {
+        fetch('./users.json')
+            .then((response) => response.json())
+            .then((users) => this.setState({users: users}))
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <h1>List of Users</h1>
+                <table className="table-striped table-condensed table table-bordered table-hover">
+                    <tbody>{this.state.users.map((user) => 
+                        <tr key={user.id}>
+                            <td>{user.first_name} {user.last_name}</td>
+                            <td>{user.email}</td>
+                            <td>{user.ip_address}</td>
+                        </tr> )}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+ReactDOM.render(
+    <Users />,
+    document.getElementById('content-2')
+)
+
+//// Adding and removing an event listener
+class Note extends React.Component {
+    confirmLeave(e) {
+        let confirmationMessage = 'Do you really wanna close?'
+        e.returnValue = confirmationMessage
+        return confirmationMessage
+    }
+    componentDidMount() {
+        console.log('Attaching confirmLeave event listener for beforeunload')
+        window.addEventListener('beforeunload', this.confirmLeave)
+    }
+    componentWillUnmount() {
+        console.log('Removing confirmLeave event listener for beforeunload')
+        window.removeEventListener('beforeunload', this.confirmLeave)
+    }
+
+    render() {
+        console.log('Rendering...')
+        return <p>Here will be our input field for notes (parent will remove in {this.props.secondsLeft} seconds)</p>
+    }
+}
+
+let secondsLeft = 5
+let interval = setInterval(() => {
+    if (secondsLeft === 0) {
+        ReactDOM.render(
+            <div>
+                Note was removed after {secondsLeft} seconds.
+            </div>,
+            document.getElementById('content-3')
+        )
+        clearInterval(interval)
+    } else {
+        ReactDOM.render(
+            <div>
+                <Note secondsLeft={secondsLeft}/>
+            </div>,
+            document.getElementById('content-3')
+        )
+    }
+    secondsLeft--
+}, 1000);
