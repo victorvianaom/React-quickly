5ce89613ad82e6f7db8d87e3670323d5ccefacde
victorvianaom <35640700+victorvianaom@users.noreply.github.com>
2020-06-13 11:49:56 -0300

    binding on render() vs binding on constructor()

--- Ch-06-Handling-Envents-in-React/js/main.js
+++ Ch-06-Handling-Envents-in-React/js/main.js
@@ -5,3 +5,38 @@ ReactDOM.render(React.createElement(
         }.bind(this) },
     'Save'
 ), document.getElementById('content-1'));
+
+/// binding on return
+class SaveButton extends React.Component {
+    handleSave(event) {
+        console.log(this, event);
+    }
+    render() {
+        return React.createElement(
+            'button',
+            { onClick: this.handleSave.bind(this) },
+            'Save'
+        );
+    }
+}
+ReactDOM.render(React.createElement(SaveButton, null), document.getElementById('content-2'));
+
+//binding on constructor
+class SaveButton2 extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleSave = this.handleSave.bind(this);
+    }
+    handleSave(event) {
+        console.log(this, event);
+    }
+
+    render() {
+        return React.createElement(
+            'button',
+            { onClick: this.handleSave },
+            'Save'
+        );
+    }
+}
+ReactDOM.render(React.createElement(SaveButton2, null), document.getElementById('content-3'));
--- Ch-06-Handling-Envents-in-React/jsx/main.jsx
+++ Ch-06-Handling-Envents-in-React/jsx/main.jsx
@@ -4,3 +4,36 @@ ReactDOM.render(
     }).bind(this)}>Save</button>,
     document.getElementById('content-1')
 )
+
+/// binding on return
+class SaveButton extends React.Component {
+    handleSave(event) {
+        console.log(this, event)
+    }
+    render() {
+        return <button onClick={this.handleSave.bind(this)} >Save</button>
+    }
+}
+ReactDOM.render(
+    <SaveButton />,
+    document.getElementById('content-2')
+)
+
+//binding on constructor
+class SaveButton2 extends React.Component {
+    constructor(props) {
+        super(props)
+        this.handleSave = this.handleSave.bind(this)
+    }
+    handleSave(event) {
+        console.log(this, event)
+    }
+
+    render() {
+        return <button onClick={this.handleSave}>Save</button>
+    }
+}
+ReactDOM.render(
+    <SaveButton2 />,
+    document.getElementById('content-3')
+)
