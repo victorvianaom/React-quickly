61816fff50661f4f4e40fea9a4e86716f092c52e
victorvianaom <35640700+victorvianaom@users.noreply.github.com>
2020-06-28 18:00:23 -0300

    handling chackbox

--- Ch-07-Working-with-Forms-in-React/js/main.js
+++ Ch-07-Working-with-Forms-in-React/js/main.js
@@ -30,11 +30,18 @@ class Form1 extends React.Component {
     constructor(props) {
         super(props);
         this.handleRadio = this.handleRadio.bind(this);
+        this.handleCheckbox = this.handleCheckbox.bind(this);
         this.state = {
             radioGroup: {
                 angular: false,
                 react: true,
                 polymer: false
+            },
+            checkboxGroup: {
+                node: true,
+                react: true,
+                express: true,
+                mongodb: true
             }
         };
     }
@@ -43,29 +50,85 @@ class Form1 extends React.Component {
         obj[event.target.value] = event.target.checked;
         this.setState({ radioGroup: obj });
     }
+    handleCheckbox(event) {
+        let obj = Object.assign(this.state.checkboxGroup);
+        obj[event.target.value] = event.target.checked;
+        this.setState({ checkboxGroup: obj });
+    }
 
     render() {
         return React.createElement(
             "form",
             null,
+            React.createElement(
+                "b",
+                null,
+                "Radio:"
+            ),
+            React.createElement("br", null),
             React.createElement("input", { type: "radio",
                 name: "radioGroup",
                 value: "angular",
                 checked: this.state.radioGroup['angular'],
                 onChange: this.handleRadio
             }),
+            " Angular ",
+            React.createElement("br", null),
             React.createElement("input", { type: "radio",
                 name: "radioGroup",
                 value: "react",
                 checked: this.state.radioGroup['react'],
                 onChange: this.handleRadio
             }),
+            " React ",
+            React.createElement("br", null),
             React.createElement("input", { type: "radio",
                 name: "radioGroup",
                 value: "polymer",
                 checked: this.state.radioGroup['polymer'],
                 onChange: this.handleRadio
-            })
+            }),
+            " Polymer ",
+            React.createElement("br", null),
+            React.createElement("br", null),
+            React.createElement(
+                "b",
+                null,
+                "Checkbox:"
+            ),
+            React.createElement("br", null),
+            React.createElement("input", { type: "checkbox",
+                name: "checkboxGroup",
+                value: "node",
+                checked: this.state.checkboxGroup['node'],
+                onChange: this.handleCheckbox
+            }),
+            " Node ",
+            React.createElement("br", null),
+            React.createElement("input", { type: "checkbox",
+                name: "checkboxGroup",
+                value: "react",
+                checked: this.state.checkboxGroup['react'],
+                onChange: this.handleCheckbox
+            }),
+            " React ",
+            React.createElement("br", null),
+            React.createElement("input", { type: "checkbox",
+                name: "checkboxGroup",
+                value: "express",
+                checked: this.state.checkboxGroup['express'],
+                onChange: this.handleCheckbox
+            }),
+            " Express ",
+            React.createElement("br", null),
+            React.createElement("input", { type: "checkbox",
+                name: "checkboxGroup",
+                value: "mongodb",
+                checked: this.state.checkboxGroup['mongodb'],
+                onChange: this.handleCheckbox
+            }),
+            " MongoDB ",
+            React.createElement("br", null)
         );
     }
 }
--- Ch-07-Working-with-Forms-in-React/jsx/main.jsx
+++ Ch-07-Working-with-Forms-in-React/jsx/main.jsx
@@ -40,11 +40,18 @@ class Form1 extends React.Component {
     constructor(props) {
         super(props)
         this.handleRadio = this.handleRadio.bind(this)
+        this.handleCheckbox = this.handleCheckbox.bind(this)
         this.state = {
             radioGroup: {
                 angular: false,
                 react: true,
                 polymer: false
+            },
+            checkboxGroup: {
+                node: true,
+                react: true,
+                express: true,
+                mongodb: true
             }
         }
     }
@@ -53,28 +60,60 @@ class Form1 extends React.Component {
         obj[event.target.value] = event.target.checked
         this.setState({radioGroup: obj})
     }
+    handleCheckbox(event) {
+        let obj = Object.assign(this.state.checkboxGroup)
+        obj[event.target.value] = event.target.checked
+        this.setState({checkboxGroup: obj})
+    }
 
     render() {
         return(
             <form>
+                <b>Radio:</b><br />
                 <input  type='radio'
                         name='radioGroup'
                         value='angular'
                         checked={this.state.radioGroup['angular']}
                         onChange={this.handleRadio} 
-                />
+                /> Angular <br />
                 <input  type='radio'
                         name='radioGroup'
                         value='react'
                         checked={this.state.radioGroup['react']}
                         onChange={this.handleRadio} 
-                />
+                /> React <br />
                 <input  type='radio'
                         name='radioGroup'
                         value='polymer'
                         checked={this.state.radioGroup['polymer']}
                         onChange={this.handleRadio} 
-                />
+                /> Polymer <br />
+                <br />
+                <b>Checkbox:</b><br />
+                <input  type='checkbox'
+                        name='checkboxGroup'
+                        value='node'
+                        checked={this.state.checkboxGroup['node']}
+                        onChange={this.handleCheckbox} 
+                /> Node <br />
+                <input  type='checkbox'
+                        name='checkboxGroup'
+                        value='react'
+                        checked={this.state.checkboxGroup['react']}
+                        onChange={this.handleCheckbox} 
+                /> React <br />
+                <input  type='checkbox'
+                        name='checkboxGroup'
+                        value='express'
+                        checked={this.state.checkboxGroup['express']}
+                        onChange={this.handleCheckbox} 
+                /> Express <br />
+                <input  type='checkbox'
+                        name='checkboxGroup'
+                        value='mongodb'
+                        checked={this.state.checkboxGroup['mongodb']}
+                        onChange={this.handleCheckbox} 
+                /> MongoDB <br />
             </form>
         )
     }
