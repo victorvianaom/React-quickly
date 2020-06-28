58e98ed956e9b0cf64b14af4a5279234dc3c848c
victorvianaom <35640700+victorvianaom@users.noreply.github.com>
2020-06-28 17:20:51 -0300

    Rendering radio buttons and handling changes

--- Ch-07-Working-with-Forms-in-React/js/main.js
+++ Ch-07-Working-with-Forms-in-React/js/main.js
@@ -24,3 +24,49 @@ class InputTextChange extends React.Component {
     }
 }
 ReactDOM.render(React.createElement(InputTextChange, null), document.getElementById('div-2'));
+
+// Rendering radio buttons and handling changes
+class Form1 extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleRadio = this.handleRadio.bind(this);
+        this.state = {
+            radioGroup: {
+                angular: false,
+                react: true,
+                polymer: false
+            }
+        };
+    }
+    handleRadio(event) {
+        let obj = {}; //erase other radios
+        obj[event.target.value] = event.target.checked;
+        this.setState({ radioGroup: obj });
+    }
+
+    render() {
+        return React.createElement(
+            "form",
+            null,
+            React.createElement("input", { type: "radio",
+                name: "radioGroup",
+                value: "angular",
+                checked: this.state.radioGroup['angular'],
+                onChange: this.handleRadio
+            }),
+            React.createElement("input", { type: "radio",
+                name: "radioGroup",
+                value: "react",
+                checked: this.state.radioGroup['react'],
+                onChange: this.handleRadio
+            }),
+            React.createElement("input", { type: "radio",
+                name: "radioGroup",
+                value: "polymer",
+                checked: this.state.radioGroup['polymer'],
+                onChange: this.handleRadio
+            })
+        );
+    }
+}
+ReactDOM.render(React.createElement(Form1, null), document.getElementById('div-3'));
--- Ch-07-Working-with-Forms-in-React/jsx/main.jsx
+++ Ch-07-Working-with-Forms-in-React/jsx/main.jsx
@@ -34,3 +34,52 @@ ReactDOM.render(
     <InputTextChange />,
     document.getElementById('div-2')
 )
+
+// Rendering radio buttons and handling changes
+class Form1 extends React.Component {
+    constructor(props) {
+        super(props)
+        this.handleRadio = this.handleRadio.bind(this)
+        this.state = {
+            radioGroup: {
+                angular: false,
+                react: true,
+                polymer: false
+            }
+        }
+    }
+    handleRadio(event) {
+        let obj = {} //erase other radios
+        obj[event.target.value] = event.target.checked
+        this.setState({radioGroup: obj})
+    }
+
+    render() {
+        return(
+            <form>
+                <input  type='radio'
+                        name='radioGroup'
+                        value='angular'
+                        checked={this.state.radioGroup['angular']}
+                        onChange={this.handleRadio} 
+                />
+                <input  type='radio'
+                        name='radioGroup'
+                        value='react'
+                        checked={this.state.radioGroup['react']}
+                        onChange={this.handleRadio} 
+                />
+                <input  type='radio'
+                        name='radioGroup'
+                        value='polymer'
+                        checked={this.state.radioGroup['polymer']}
+                        onChange={this.handleRadio} 
+                />
+            </form>
+        )
+    }
+}
+ReactDOM.render(
+    <Form1 />,
+    document.getElementById('div-3')
+)
